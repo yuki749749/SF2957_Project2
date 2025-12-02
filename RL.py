fe3b5ac2748bcdc9d3e8e330481d25eeb589c2f5
yuki749749 <yuki749749@gmail.com>
2025-12-02 22:28:03 +0100

    Updated MC-learning

--- RL.py
+++ RL.py
@@ -193,15 +193,15 @@ def learn_MC(env, n_sims, gamma = 1, epsilon = 0.05,
         # Game (episode) is over
      #################################################################### 
         # YOUR CODE HERE
-            # Update avg_reward
-            avg_reward += (episode_reward - avg_reward) / (episode + 1)
+        # Update avg_reward
+        avg_reward += (episode_reward - avg_reward) / (episode + 1)
 
-            # Update the Q-function for each visited state/action pair. 
-            for s in episode_state_action_count:
-                for a in range(env.action_space.n):
-                    if episode_state_action_count[s][a] > 0:
-                        alpha = 1 / (state_action_count[s][a] ** 0.77)
-                        Q[s][a] += alpha * (episode_reward - Q[s][a])
+        # Update the Q-function for each visited state/action pair. 
+        for s, counts in episode_state_action_count.items():
+            for a, c in enumerate(counts):
+                if c > 0:
+                    n = state_action_count[s][a]
+                    Q[s][a] += (episode_reward - Q[s][a]) / n
 
         
 
